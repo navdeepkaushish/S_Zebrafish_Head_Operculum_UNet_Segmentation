86ab27b1e68c8dc35d81be486d8f6c5b894dcdfc
navdeepkaushish <nkumar@uliege.be>
2021-04-08 10:03:07 +0200

    Update run.py

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def main(argv):
             h_model = load_model('/models/head_dice_9974.hdf5', compile=False)  # head model
             h_model.compile(optimizer='adam', loss=dice_coef_loss,
                             metrics=['accuracy'])
-            op_model = load_model('/models/op_dice_9991.hdf5')  # operculum model
+            op_model = load_model('/models/op_dice_9991.hdf5', compile=False)  # operculum model
             op_model.compile(optimizer='adam', loss=dice_coef_loss,
                             metrics=['accuracy'])
 
